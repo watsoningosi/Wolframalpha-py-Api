5b6ce68d5bc88791001c889ff25470d54aaadc93
watsoningosi <watstarsparta@gmail.com>
2021-06-19 12:32:57 +0300

    WolframAlpha

---
+++ pyda.py
@@ -0,0 +1,40 @@
+import wolframalpha
+client = wolframalpha.Client("Y76R37-VPE9P28TK4")
+
+import wikipedia
+
+import PySimpleGUI as sg
+sg.theme('DarkPurple')
+layout =[[sg.Text('Enter a command'), sg.InputText()],[sg.Button('Ok'), sg.Button('Cancel')]]
+window = sg.Window('PyDa', layout)
+
+import pyttsx3
+engine = pyttsx3.init()
+
+while True:
+    event, values = window.read()
+    if event in (None, 'Cancel'):
+        break
+    try:
+        wiki_res = wikipedia.summary(values[0], sentences=2)
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking("Wolfram Result: "+wolfram_res,"Wikipedia Result: "+wiki_res)
+    except wikipedia.exceptions.DisambiguationError:
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking(wolfram_res)
+    except wikipedia.exceptions.PageError:
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking(wolfram_res)
+    except:
+        wiki_res = wikipedia.summary(values[0], sentences=2)
+        engine.say(wiki_res)
+        sg.PopupNonBlocking(wiki_res)
+
+    engine.runAndWait()
+
+    print (values[0])
+
+window.close()
